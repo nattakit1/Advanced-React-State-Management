01a425302a028cfde2b5dbe0d932ec52d8b030dd
nattakit1 <nattakit_ka67@live.rmutl.ac.th>
2025-09-03 20:16:32 +0700

    update web

--- mini-ecommerce/src/main.jsx
+++ mini-ecommerce/src/main.jsx
@@ -1,19 +1,18 @@
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom"; // <-- เปลี่ยนตรงนี้
+import { HashRouter } from "react-router-dom"; // <-- เปลี่ยนจาก BrowserRouter
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
 // import CSS
-import "./index.css";  // <--- ต้องมีถ้าใช้ global CSS
-import "./App.css";    // <--- ถ้ามีไฟล์ CSS ของ App
+import "./index.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>  {/* <-- เปลี่ยนจาก BrowserRouter */}
+      <HashRouter>
         <App />
       </HashRouter>
     </Provider>
--- mini-ecommerce/vite.config.js
+++ mini-ecommerce/vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Advanced-React-State-Management/mini-ecommerce/", // <-- ชื่อ repo ของคุณ
+  base: process.env.NODE_ENV === "production" 
+        ? "/Advanced-React-State-Management/mini-ecommerce/" 
+        : "/", // dev ใช้ localhost ปกติ
 });
